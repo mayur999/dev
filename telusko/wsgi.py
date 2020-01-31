9f58592286f3805fd055d446f3f44d70b00bc7f1
Siji Menon <siji.menon@northgateps.com>
2020-01-31 20:58:48 +0530

    project name changed.

--- telusko/wsgi.py
+++ telusko/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travello.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telusko.settings')
 
 application = get_wsgi_application()
